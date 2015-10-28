7cd6a5ad624389b1164a2d7b955f4e287a66d49f
= <=>
2015-10-28 12:17:12 +0100

    no message

--- src/vss/a3/VssA3.java
+++ src/vss/a3/VssA3.java
@@ -16,8 +16,6 @@ public class VssA3 {
      */
     public static void main(String[] args) {
         System.out.println("Hallo Timmy!");
-        System.out.println("TEST2");
-        System.out.println("");
     }
     
 }
