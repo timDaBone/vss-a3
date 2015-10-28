0797838f73b3d830b34a3d21885f81ddd839d97c
Andi <a.buchmann@online.de>
2015-10-28 10:52:03 +0100

    Test

--- src/vss/a3/VssA3.java
+++ src/vss/a3/VssA3.java
@@ -15,7 +15,7 @@ public class VssA3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hallo Timmy!");
     }
     
 }
